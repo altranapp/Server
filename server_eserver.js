7a6f5df564a355c9956b9340335a051c65ea7209
altranapp <brandygoodluck11@gmail.com>
2026-04-22 15:05:29 +0100

    Update server_eserver.js

--- server_eserver.js
+++ server_eserver.js
@@ -1,1 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("DB Connected"))
+.catch(err=>console.log(err));
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/user', require('./routes/user'));
+app.use('/api/admin', require('./routes/admin'));
+app.use('/api/kyc', require('./routes/kyc'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
