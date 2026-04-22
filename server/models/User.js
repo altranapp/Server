fa54cceef1f090bdba135ba520978b109293bfe3
altranapp <brandygoodluck11@gmail.com>
2026-04-22 20:12:16 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,55 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    password: {
-      type: String,
-      required: true,
-    },
-
-    role: {
-      type: String,
-      default: "user",
-    },
-
-    balance: {
-      type: Number,
-      default: 0,
-    },
-
-    // KYC fields
-    name: {
-      type: String,
-      default: "",
-    },
-
-    country: {
-      type: String,
-      default: "",
-    },
-
-    phone: {
-      type: String,
-      default: "",
-    },
-
-    sex: {
-      type: String,
-      default: "",
-    },
-
-    kycStatus: {
-      type: String,
-      default: "not_submitted", // not_submitted | pending | approved | rejected
-    },
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    default: 0
+  },
+  role: {
+    type: String,
+    default: "user"
+  },
+  kycStatus: {
+    type: String,
+    default: "not_submitted"
+  },
+  name: String,
+  country: String,
+  phone: String,
+  sex: String
+}, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
