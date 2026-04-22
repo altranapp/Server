953bacfbc116989f39f232021ce5f26008d2f779
altranapp <brandygoodluck11@gmail.com>
2026-04-22 14:51:01 +0100

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const User = require('../models/User');
-
-router.get('/balance/:id', async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.json({ balance: user.balance });
-});
-
-module.exports = router;
