3b1e2cba1c3fc60ad00a71cd0cb070174bc9c563
altranapp <brandygoodluck11@gmail.com>
2026-04-23 19:05:49 +0100

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,21 @@ import { protect, adminOnly } from "../middleware/auth.js";
 
 const router = express.Router();
 
-// GET ALL USERS
+// Get all users
 router.get("/users", protect, adminOnly, async (req, res) => {
-  const users = await User.find().select("-password");
+  const users = await User.find();
   res.json(users);
+});
+
+// Approve deposit (example)
+router.post("/deposit", protect, adminOnly, async (req, res) => {
+  const { userId, amount } = req.body;
+
+  const user = await User.findById(userId);
+  user.balance += amount;
+  await user.save();
+
+  res.json({ message: "Deposit approved" });
 });
 
 export default router;
